832dfc73797120eb1b6d42ddf4c75f76489396a7
Raghav Bansal <raghavbansal347@gmail.com>
2019-03-19 11:09:01 +0530

    all pages

--- index.js
+++ index.js
@@ -95,3 +95,20 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
+$(document).ready(function(){
+	$(".ct-btn-scroll").on('click', function(event) {
+	  if (this.hash !== "") {
+		event.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+		  scrollTop: $(hash).offset().top
+		}, 800, function(){
+		  window.location.hash = hash;
+		});
+	  } 
+	});
+	  
+	setInterval(function(){
+	  $('.ct-btn-scroll').toggleClass("bounce");
+	}, 5000); 
+});
---
+++ portfolio.js
@@ -0,0 +1,14 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+$(document).ready(function(){
+// init Masonry
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  // layout Masonry after each image loads
+//   $grid.imagesLoaded().progress( function() {
+//     $grid.masonry();
+//   });  
+
+});
